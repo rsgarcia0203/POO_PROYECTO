66149f84a40fe0af583de03dad677809d53cd58f
stephcarol <85514342+stephcarol@users.noreply.github.com>
2021-07-05 22:13:21 -0500

    Update Automovil.java

--- src/main/java/ec/edu/espol/model/Automovil.java
+++ src/main/java/ec/edu/espol/model/Automovil.java
@@ -5,6 +5,11 @@
  */
 package ec.edu.espol.model;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /**
  *
  * @author rsgar
@@ -13,8 +18,8 @@ public class Automovil extends Vehiculo{
     private String vidrios;
     private String transmision;
     
-    public Automovil(String placa, String marca, String modelo, String tipo_motor, int año, double recorrido, String color, String tipo_combustible, String vidrios, String transmision, String traccion, double precio){
-        super(placa, marca, modelo, tipo_motor, año, recorrido, color, tipo_combustible, precio);
+    public Automovil(String placa, String marca, String modelo, String tipo_motor, int anio, double recorrido, String color, String tipo_combustible, String vidrios, String transmision, double precio){
+        super(placa, marca, modelo, tipo_motor, anio, recorrido, color, tipo_combustible, precio);
         this.vidrios = vidrios;
         this.transmision = transmision;
     }
@@ -34,6 +39,37 @@ public class Automovil extends Vehiculo{
     public void setTransmision(String transmision) {
         this.transmision = transmision;
     }
+
+    
+    public static void nextAutomovil(Scanner sc, String nomfile){
+        System.out.println("Ingrese la placa: ");
+        String placa = sc.next();
+        System.out.println("Ingrese la marca: ");
+        String marca = sc.next();
+        System.out.println("Ingrese el Modelo: ");
+        String modelo = sc.next();
+        System.out.println("Ingrese el tipo de motor: ");
+        String tipodeMotor = sc.next();
+        System.out.println("Ingrese el anio: ");
+        int anio = sc.nextInt();
+        System.out.println("Ingrese el recorrido: ");
+        double recorrido =sc.nextDouble();
+        System.out.println("Ingrese el color: ");
+        String color =sc.next();
+        System.out.println("Ingrese el tipo de combustible: ");
+        String TipoCombustible =sc.next();
+        System.out.println("Ingrese el tipo de vidrio: ");
+        String vidrio =sc.next();
+        System.out.println("Ingrese el tipo de transmision: ");
+        String transmision =sc.next();
+        System.out.println("Ingrese el precio: ");
+        double precio =sc.nextDouble();
+        Automovil m=new Automovil(placa,marca, modelo, tipodeMotor, anio, recorrido,color,TipoCombustible,vidrio,transmision,precio);
+        
+    }
+    
+    
+    
     public void saveFile(String nomfile){
         try(PrintWriter pw = new PrintWriter(new FileOutputStream(new File(nomfile),true)))
         {
