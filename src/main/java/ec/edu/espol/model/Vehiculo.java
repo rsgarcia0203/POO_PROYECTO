9bdd29e2fae9fc25b729baa8457a8ef7cb3dc40e
stephcarol <85514342+stephcarol@users.noreply.github.com>
2021-07-05 21:11:48 -0500

    Update Vehiculo.java

--- src/main/java/ec/edu/espol/model/Vehiculo.java
+++ src/main/java/ec/edu/espol/model/Vehiculo.java
@@ -5,6 +5,11 @@
  */
 package ec.edu.espol.model;
 
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
