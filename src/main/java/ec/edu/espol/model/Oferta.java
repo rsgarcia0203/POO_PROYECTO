aa4f0ee547777ab13c0ed4c6f76218638bc30f0e
rsgarcia0203 <rsgarcia@espol.edu.ec>
2021-07-11 00:11:55 -0500

    Update Oferta.java

--- src/main/java/ec/edu/espol/model/Oferta.java
+++ src/main/java/ec/edu/espol/model/Oferta.java
@@ -18,17 +18,17 @@ import java.util.Scanner;
  */
 public class Oferta {
     private int ID;
-    private int IDvendedor;
     private int IDvehiculo;
     private int IDtipo; //guardaremos el ID del tipo dependiendo del tipo
-    private String correo;
+    private int IDcomprador;
+    private Vehiculo vehiculo;
+    private Comprador comprador;
     
-    public Oferta(int ID, int IDvendedor, int IDvehiculo, int IDtipo, String correo){
+    public Oferta(int ID, int IDvehiculo, int IDtipo, int IDcomprador){
         this.ID = ID;
-        this.IDvendedor = IDvendedor;
         this.IDvehiculo = IDvehiculo;
         this.IDtipo = IDtipo;
-        this.correo = correo;
+        this.IDcomprador = IDcomprador;
     }
 
     public int getID() {
@@ -37,14 +37,6 @@ public class Oferta {
 
     public void setID(int ID) {
         this.ID = ID;
-    }
-
-    public int getIDvendedor() {
-        return this.IDvendedor;
-    }
-
-    public void setIDvendedor(int IDvendedor) {
-        this.IDvendedor = IDvendedor;
     }
 
     public int getIDvehiculo() {
@@ -62,20 +54,36 @@ public class Oferta {
     public void setIDtipo(int IDtipo) {
         this.IDtipo = IDtipo;
     }
-    
-    public String getCorreo() {
-        return this.correo;
+
+    public int getIDcomprador() {
+        return IDcomprador;
     }
 
-    public void setCorreo(String correo) {
-        this.correo = correo;
+    public void setIDcomprador(int IDcomprador) {
+        this.IDcomprador = IDcomprador;
+    }
+    
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public Comprador getComprador() {
+        return comprador;
+    }
+
+    public void setComprador(Comprador comprador) {
+        this.comprador = comprador;
     }
     
     public void saveFile(String nomfile, String correo)
     {
         try(PrintWriter pw = new PrintWriter(new FileOutputStream(new File(nomfile),true)))
         {
-            pw.println(this.ID+"|"+this.IDvendedor+"|"+this.IDvehiculo+"|"+correo);
+            pw.println(this.ID+"|"+this.IDvehiculo+"|"+this.IDtipo+"|"+this.IDcomprador);
         }
         catch(Exception e)
         {
@@ -90,7 +98,7 @@ public class Oferta {
             while (sc.hasNextLine()){
                 String linea = sc.next();
                 String [] arreglo = linea.split("\\|");
-                Oferta o = new Oferta(Integer.parseInt(arreglo[0]), Integer.parseInt(arreglo[1]), Integer.parseInt(arreglo[2]), Integer.parseInt(arreglo[3]), arreglo[4]);
+                Oferta o = new Oferta(Integer.parseInt(arreglo[0]), Integer.parseInt(arreglo[1]), Integer.parseInt(arreglo[2]), Integer.parseInt(arreglo[3]));
                 oferta.add(o);
             }
         }catch(Exception ex){
@@ -101,34 +109,51 @@ public class Oferta {
     
     @Override
     public String toString() {
-        return  this.ID + ", " + this.IDvendedor + ", " + this.IDvehiculo + ", " + this.IDtipo + ", " + this.correo;
+        return  this.ID + ", " + this.IDvehiculo + ", " + this.IDtipo + ", " + this.IDcomprador;
     }
     
     public static Oferta registrarNuevaOferta(Vehiculo vehiculo, Comprador comprador, String nomfile)
      {
         int id = Util.nextID(nomfile);
-        int IDvendedor = vehiculo.getIDvendedor();
         int IDvehiculo = vehiculo.getIDvehiculo();
         int IDtipo = 0;
+        int IDcomprador = comprador.getID();
         if (vehiculo instanceof Automovil)
             IDtipo = 1; 
         else if (vehiculo instanceof Motocicleta)
             IDtipo = 2; 
         else if (vehiculo instanceof Camioneta)
             IDtipo = 3; 
-        String correo = comprador.getCorreo();
-        Oferta nuevo = new Oferta(id,IDvendedor,IDvehiculo, IDtipo,correo);
+        Oferta nuevo = new Oferta(id,IDvehiculo, IDtipo, IDcomprador);
         return nuevo;
     }
     
-    public static void link(ArrayList<Vendedor> vendedores, ArrayList<Vehiculo> vehiculos, ArrayList<Ingreso> ingresos){
-        for(Ingreso i: ingresos){
-            //Vehiculo vh = Vehiculo.searchByID(vehiculos, i.getIdVehiculo());
-            //Vendedor v = Vendedor.searchByID(vendedores, i.getIdVendedor());
-            //v.getVehiculos().add(vh);
-            //.setVendedor(v);
-            //i.setVendedor(v);
-            //i.setVehiculo(vh);
+    public static void link(ArrayList<Comprador> compradores, ArrayList<Automovil> automoviles, ArrayList<Camioneta> camionetas, ArrayList<Motocicleta> motocicletas, ArrayList<Oferta> ofertas){
+        for(Oferta o: ofertas){
+            Comprador c = Comprador.searchByID(compradores, o.getIDcomprador());
+            switch(o.getIDtipo())
+            {
+                case 1:
+                    Automovil au = Automovil.searchByID(automoviles, o.getIDvehiculo());
+                    au.getOfertas().add(o);
+                    o.setComprador(c);
+                    o.setVehiculo(au);
+                    break; 
+                   
+                case 2:
+                    Motocicleta mo = Motocicleta.searchByID(motocicletas, o.getIDvehiculo());
+                    mo.getOfertas().add(o);
+                    o.setComprador(c);
+                    o.setVehiculo(mo);
+                    break; 
+                    
+                case 3:
+                    Camioneta ca = Camioneta.searchByID(camionetas, o.getIDvehiculo());
+                    ca.getOfertas().add(o);
+                    o.setComprador(c);
+                    o.setVehiculo(ca);
+                    break; 
+            }                      
         }
     }
 }
